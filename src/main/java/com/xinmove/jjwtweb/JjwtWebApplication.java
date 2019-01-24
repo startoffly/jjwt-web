72f8d069b3564583143a8398d4f56526df161bdd
chenweitao <chenweitao@zhiweidata.com>
2019-01-24 18:10:41 +0800

    first commit

---
+++ src/main/java/com/xinmove/jjwtweb/JjwtWebApplication.java
@@ -0,0 +1,14 @@
+package com.xinmove.jjwtweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JjwtWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JjwtWebApplication.class, args);
+    }
+
+}
+
---
+++ src/main/java/com/xinmove/jjwtweb/controller/UserController.java
@@ -0,0 +1,15 @@
+package com.xinmove.jjwtweb.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @ClassName UserController
+ * @Descripption TODO
+ * @Author chenweitao
+ * @Date 2019/1/24 17:46
+ **/
+@Controller
+@RequestMapping("/api/user")
+public class UserController {
+}
